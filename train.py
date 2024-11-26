1fa2f55eabbf6bca5f306a5b222dbe1259bfb8b7
jordanxlau <jordanxlau@yahoo.ca>
2024-11-25 21:55:02 -0500

    New Functionality: No longer using CNN, as data is no longer in pixel form

--- train.py
+++ train.py
@@ -1,27 +1,26 @@
-from preprocessing import X_train, X_test, y_train, y_test
 import keras
-from functools import partial
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
 
-# The architecture of this Neural Network was been borrowed from my CSI 4106 lecture notes
+X = pd.read_csv("X.csv")
+y = pd.read_csv("y.csv")
 
-# Create a Convolutional Neural Network
-DefaultConv2D = partial(keras.layers.Conv2D, kernel_size=3, padding="same", activation="relu", kernel_initializer="he_normal")
+# Randomly shuffle and split data into training and validation
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1, shuffle=True)
 
+# Convert to Tensorflow Tensor
+X_train, X_test, y_train, y_test = tf.constant(X_train), tf.constant(X_test), tf.constant(y_train), tf.constant(y_test)
+
+# Create a Neural Network
 model = keras.Sequential([
-  DefaultConv2D(filters=64, kernel_size=3, input_shape=(28, 28, 1)),
-  keras.layers.MaxPool2D(),
-  DefaultConv2D(filters=128),
-  DefaultConv2D(filters=128),
-  keras.layers.MaxPool2D(),
-  DefaultConv2D(filters=256),
-  DefaultConv2D(filters=256),
-  keras.layers.MaxPool2D(),
+  keras.layers.InputLayer(shape=(63,)),
   keras.layers.Flatten(),
   keras.layers.Dense(units=128, activation="relu", kernel_initializer="he_normal"),
   keras.layers.Dropout(0.10),
   keras.layers.Dense(units=64, activation="relu", kernel_initializer="he_normal"),
   keras.layers.Dropout(0.10),
-  keras.layers.Dense(units=26, activation="softmax")
+  keras.layers.Dense(units=5, activation="softmax")
 ])
 
 # Compile the model
@@ -36,4 +35,4 @@ history = model.fit(X_train, y_train, epochs=20, validation_data=(X_test, y_test
 model.summary()
 
 # Save
-model.save("model-wholealphabet.keras", overwrite=True)
+model.save("model-fivesymbols.keras", overwrite=True)
