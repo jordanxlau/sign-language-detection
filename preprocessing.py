94c180c10eb29fff7ea566f16060cc4a61a85203
jordanxlau <jordanxlau@yahoo.ca>
2024-11-25 21:43:14 -0500

    New Functionality: Hand Gestures are now stored/procesed as hand landmarks through Google Mediapipe, rather than as pixels

--- preprocessing.py
+++ preprocessing.py
@@ -1,45 +1,96 @@
+from mediapipe import solutions
+import cv2
 import pandas as pd
-import numpy as np
+import os
 
-# Download sign language dataset from: https://www.kaggle.com/datasets/datamunge/sign-language-mnist
-test = pd.read_csv( "C:/Users/jorda/.cache/kagglehub/datasets/datamunge/sign-language-mnist/versions/1/sign_mnist_test.csv" )
-train = pd.read_csv( "C:/Users/jorda/.cache/kagglehub/datasets/datamunge/sign-language-mnist/versions/1/sign_mnist_train.csv" )
+# Dataset from: https://github.com/hukenovs/hagrid/tree/Hagrid_v2?tab=readme-ov-file
+labels = {
+    0: 'call', 1: 'dislike', 2: 'fist', 3: 'four', 4: 'like'
+}
 
-# Uncomment these lines to predict only a, b, c, d and e
-# train = train[train['label'] < 5]
-# test = test[test['label'] < 5]
+try:
+    X = [] # Processed images (each is a list of 21 3D landmark points)
+    y = [] # Class labels
 
-# Isolate X and y
-X_train = train.values[:,1:]
-y_train = train.values[:,0]
-X_test = test.values[:,1:]
-y_test = test.values[:,0]
+    # Convert each image in the dataset to hand landmarks using mediapipe
+    mp_drawing = solutions.drawing_utils
+    mp_drawing_styles = solutions.drawing_styles
+    mp_hands = solutions.hands
 
-# Reshape data
-X_train = X_train.reshape(27455, 28, 28)
-X_test = X_test.reshape(7172, 28, 28)
-# Uncomment these lines to predict only a, b, c, d and e
-# X_train = X_train.reshape(5433, 28, 28)
-# X_test = X_test.reshape(1816, 28, 28)
+    # Define a mediapipe Hands object
+    hands = mp_hands.Hands(
+        static_image_mode=True,
+        max_num_hands=1,
+        min_detection_confidence=0.25)
 
-# Convert data to cv2-acceptable datatype
-X_train = X_train.astype(np.uint8)
-X_test = X_test.astype(np.uint8)
+    # Read data for each class
+    for i in range(len(labels)):
+        label = labels[i]
+        folder = os.listdir("hagrid_dataset_512/" + label)
+        print("label:", label)
+        counter = 0
 
-# Uncomment these lines to perform Data Augmentation by adding noisy examples
-# original_len = len(X_train)
-# for i in range( original_len ):
-#     image = X_train[i]
+        # Read all images in the folder for class i
+        for image_name in folder:
+            # Only process the first 1000 examples
+            if counter > 1000:
+                break
+            else:
+                counter+=1
+            
+            image = cv2.imread("hagrid_dataset_512/" + label + "/" + image_name)
 
-#     noise = np.random.normal(2, 4, image.shape).astype(np.uint8)
+            # cv2.imshow("Trace Image", image)
+            # cv2.waitKey(0)
 
-#     # Add the noise to the image
-#     noisy_image = cv2.add(image, noise)
+            # Flip image for "selfie" view
+            image = cv2.flip(image, 1)
+            
+            # To improve performance, optionally mark the image as not writeable to pass by reference.
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = hands.process(image) # Process any hands in the image
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-#     X_train = np.append(X_train, np.array([noisy_image]), axis=0)
-#     y_train = np.append(y_train, y_train[i])
+            # If no hands detected, move to next image
+            if not results.multi_hand_landmarks:
+                continue
 
-print(X_train)
-print(X_test)
-print(y_train)
-print(y_test)
+            # Get landmarks from first hand detected 
+            hand_landmarks = results.multi_hand_landmarks[0]
+
+            # Trace by showing image
+            # annotated_image = image.copy()
+            # mp_drawing.draw_landmarks(
+            #     annotated_image,
+            #     hand_landmarks,
+            #     mp_hands.HAND_CONNECTIONS,
+            #     mp_drawing_styles.get_default_hand_landmarks_style(),
+            #     mp_drawing_styles.get_default_hand_connections_style())
+            # cv2.imshow("Trace Annotated Image", annotated_image)
+            # cv2.waitKey(0)
+            
+            # Group all landmark points into one "hand"
+            hand = []
+            for landmark in hand_landmarks.landmark:
+                # print([landmark.x, landmark.y, landmark.z])
+                hand.append(landmark.x)
+                hand.append(landmark.y)
+                hand.append(landmark.z)
+
+            # Add current hand example to processed data set
+            X.append(hand)
+            y.append(i)
+            print("\n\n\n\nhand:", hand)
+
+    X=pd.DataFrame(X)
+    y=pd.DataFrame(y)
+    print(X)
+    print(y)
+
+    X.to_csv("X.csv", index=False)
+    y.to_csv("y.csv", index=False)
+
+except Exception as e:
+    print("Exception:", e)
