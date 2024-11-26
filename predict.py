44e001235dfd9a9f9b7a800e543db9fbb81f3be0
jordanxlau <jordanxlau@yahoo.ca>
2024-11-25 22:24:06 -0500

    Bugfix: minor readability fixes

--- predict.py
+++ predict.py
@@ -4,6 +4,11 @@ import tensorflow as tf
 import numpy as np
 from mediapipe import solutions
 
+# List of labels
+labels = {
+    -1:'No Hand Detected', 0: 'call', 1: 'dislike', 2: 'fist', 3: 'four', 4: 'like'
+}
+
 # Find the best prediction
 def best_prediction(predictions:np.array):
     # Search an np array of normalized predictions for the max value (most likely prediction)
@@ -14,13 +19,7 @@ def best_prediction(predictions:np.array):
     
     return max
 
-# List of labels 
-labels = {
-    -1:'No Hand Detected', 0: 'call', 1: 'dislike', 2: 'fist', 3: 'four', 4: 'like'
-}
-
 try:
-        
     # Rename mediapipe utilities
     mp_drawing = solutions.drawing_utils
     mp_drawing_styles = solutions.drawing_styles
@@ -31,7 +30,7 @@ try:
 
     # Initialize video capture from webcam
     cap = cv2.VideoCapture(0)
-    success, display = cap.read()
+    success, image = cap.read()
 
     # Get info about VideoCapture
     width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
@@ -53,21 +52,24 @@ try:
         if not success:
             continue # 'break' for videos, 'continue' for webcams
 
+        # Check for esc key
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+
         # Initialize a default prediction
         prediction = -1
 
-        # Flip the image horizontally for a selfie-view display.
-        display = cv2.flip(display, 1)
+        # Flip the image horizontally for a selfie-view image.
+        image = cv2.flip(image, 1)
 
         # To improve performance, optionally mark the image as not writeable to pass by reference.
-        display.flags.writeable = False
-        display = cv2.cvtColor(display, cv2.COLOR_BGR2RGB)
-        results = hands.process(display)
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = hands.process(image)
         # Draw the hand annotations on the image.
-        display.flags.writeable = True
-        display = cv2.cvtColor(display, cv2.COLOR_RGB2BGR)
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         if results.multi_hand_landmarks:
-            print("HEYO")
             for hand_landmarks in results.multi_hand_landmarks:
                 hand = []
                 for landmark in hand_landmarks.landmark:
@@ -76,28 +78,21 @@ try:
                     hand.append(landmark.y)
                     hand.append(landmark.z)
                 # Make a prediction
-                print(hand)
                 hand = tf.constant([hand])
-                print(hand.shape)
                 predictions = model.predict(hand)
-                print("DID WE MAKE IT HERE")
                 prediction = best_prediction(predictions)
 
         # Draw a green text prediction
-        cv2.putText(display, labels[prediction], (10, 100), cv2.FONT_HERSHEY_DUPLEX, 4, (255, 200, 50), 2, cv2.LINE_AA)
+        cv2.putText(image, labels[prediction], (10, 100), cv2.FONT_HERSHEY_DUPLEX, 4, (255, 200, 50), 2, cv2.LINE_AA)
 
         # Write to output video
         # result.write(image)
 
-        # Re-display the image
-        cv2.imshow('Sign Language Alphabet Detection', display)
-        
-        # Check for esc key
-        if cv2.waitKey(5) & 0xFF == 27:
-            break
+        # Re-image the image
+        cv2.imshow('Sign Language Alphabet Detection', image)
 
         # Read next frame
-        success, display = cap.read()
+        success, image = cap.read()
 
     # result.release()
     cap.release()
